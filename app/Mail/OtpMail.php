2c0b09250d03dadb3e18dba81f7b90a1aaa2f1ba
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2025-12-31 15:18:01 +0600

    update

--- app/Mail/OtpMail.php
+++ app/Mail/OtpMail.php
@@ -9,10 +9,12 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
+// class OtpMail extends Mailable implements ShouldQueue
 class OtpMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
+    
     /**
      * Create a new message instance.
      */
