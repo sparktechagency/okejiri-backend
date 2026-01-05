c500667911f1b8102781f042cf9fa0b9b33f7ffa
Sazzat-UGV <asikulislamsazzat@gmail.com>
2026-01-05 15:20:33 +0600

    update email

--- config/mail.php
+++ config/mail.php
@@ -37,17 +37,16 @@ return [
 
     'mailers' => [
 
-        'smtp' => [
-            'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
-            'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', '127.0.0.1'),
-            'port' => env('MAIL_PORT', 2525),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
-        ],
+ 'smtp' => [
+    'transport' => 'smtp',
+    'host' => 'server237.web-hosting.com', // সরাসরি হোস্ট বসানো হলো
+    'port' => 465,
+    'encryption' => 'ssl',
+    'username' => 'noreply@okejiri.com', // সরাসরি ইমেইল
+    'password' => 'OkejiriPass2026',     // সরাসরি পাসওয়ার্ড
+    'timeout' => null,
+    'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+],
 
         'ses' => [
             'transport' => 'ses',
