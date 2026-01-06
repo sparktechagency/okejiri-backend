84c3edfb3baa7124360ca326b35d2d8762ee7bc3
Sazzat-UGV <asikulislamsazzat@gmail.com>
2026-01-06 15:58:39 +0600

    update smtp

--- config/mail.php
+++ config/mail.php
@@ -38,20 +38,21 @@ return [
     'mailers' => [
 
         'smtp'       => [
-            'transport'    => 'smtp',
-            'scheme'       => env('MAIL_SCHEME'),
-            'url'          => env('MAIL_URL'),
-            'host'         => env('MAIL_HOST', '127.0.0.1'),
-            'port'         => env('MAIL_PORT', 2525),
-            'username'     => env('MAIL_USERNAME'),
-            'password'     => env('MAIL_PASSWORD'),
-            'timeout'      => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
-            'context'      => [
+            'transport'  => 'smtp',
+            'scheme'     => env('MAIL_SCHEME'),
+            'url'        => env('MAIL_URL'),
+            'host'       => env('MAIL_HOST', '127.0.0.1'),
+            'port'       => env('MAIL_PORT', 2525),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username'   => env('MAIL_USERNAME'),
+            'password'   => env('MAIL_PASSWORD'),
+            'timeout'    => null,
+            // 'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            'stream'     => [
                 'ssl' => [
+                    'allow_self_signed' => true,
                     'verify_peer'       => false,
                     'verify_peer_name'  => false,
-                    'allow_self_signed' => true,
                 ],
             ],
         ],
